ae208ba4c9cfbc24eb47172ce0e8994763182f4f
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-02-11 17:06:24 +0300

    add like repository

---
+++ src/main/java/com/example/twitter/repository/LikeRepository.java
@@ -0,0 +1,12 @@
+package com.example.twitter.repository;
+
+import com.example.twitter.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like,Long> {
+
+    Optional<Like> findByTweetTweetIdAndUserUserId(Long tweetId, Long userId);
+
+}

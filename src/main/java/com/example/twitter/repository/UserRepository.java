6c3b42a909acaec6ee98914be79fece3d0af079b
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-02-11 17:06:03 +0300

    add user repository

---
+++ src/main/java/com/example/twitter/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.twitter.repository;
+
+import com.example.twitter.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findbyEmail(String email);
+
+}

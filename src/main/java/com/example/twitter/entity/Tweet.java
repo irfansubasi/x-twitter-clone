7e0878df432efd01a1e5542cb1952405adbc1bb1
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-02-11 15:07:47 +0300

    add tweet entity

---
+++ src/main/java/com/example/twitter/entity/Tweet.java
@@ -0,0 +1,48 @@
+package com.example.twitter.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tweets")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+
+    @Column(name = "content")
+    @NotNull(message = "Content can not be null")
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}

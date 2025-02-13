035cd2510cf90ecf6d897007536f7c4951622e44
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-02-13 03:42:02 +0300

    change retweet service

--- src/main/java/com/example/twitter/service/RetweetServiceImpl.java
+++ src/main/java/com/example/twitter/service/RetweetServiceImpl.java
@@ -2,15 +2,18 @@ package com.example.twitter.service;
 
 import com.example.twitter.entity.Retweet;
 import com.example.twitter.repository.RetweetRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
-public class RetweetServiceImpl implements RetweetService{
+public class RetweetServiceImpl implements RetweetService {
 
-    private RetweetRepository retweetRepository;
+    private final RetweetRepository retweetRepository;
 
     @Autowired
     public RetweetServiceImpl(RetweetRepository retweetRepository) {
@@ -18,13 +21,29 @@ public class RetweetServiceImpl implements RetweetService{
     }
 
     @Override
+    @Transactional
     public Retweet retweet(Retweet retweet) {
+        Optional<Retweet> existingRetweet = retweetRepository.findByTweetTweetIdAndUserUserId(
+                retweet.getTweet().getTweetId(),
+                retweet.getUser().getUserId()
+        );
+
+        if (existingRetweet.isPresent()) {
+            log.debug("Retweet zaten var");
+            return existingRetweet.get();
+        }
+
         return retweetRepository.save(retweet);
     }
 
     @Override
+    @Transactional
     public void deleteRetweet(Long tweetId, Long userId) {
+        log.debug("Şu tweet için retweet siliniyor: {} bu kullanıcı tarafından: {}", tweetId, userId);
         Optional<Retweet> retweet = retweetRepository.findByTweetTweetIdAndUserUserId(tweetId, userId);
-        retweet.ifPresent(retweetRepository::delete);
+        retweet.ifPresent(value -> {
+            log.debug("retweet bulundu. Siliniyor");
+            retweetRepository.delete(value);
+        });
     }
 }

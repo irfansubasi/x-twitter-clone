88bb6ce36d770e15847c00a1fd974cfed87e75f7
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-02-13 03:40:40 +0300

    change like service

--- src/main/java/com/example/twitter/service/LikeServiceImpl.java
+++ src/main/java/com/example/twitter/service/LikeServiceImpl.java
@@ -1,18 +1,20 @@
 package com.example.twitter.service;
 
-
 import com.example.twitter.entity.Like;
 import com.example.twitter.repository.LikeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
-public class LikeServiceImpl implements LikeService{
+public class LikeServiceImpl implements LikeService {
 
-    private LikeRepository likeRepository;
+    private final LikeRepository likeRepository;
 
     @Autowired
     public LikeServiceImpl(LikeRepository likeRepository) {
@@ -20,13 +22,30 @@ public class LikeServiceImpl implements LikeService{
     }
 
     @Override
+    @Transactional
     public Like likeTweet(Like like) {
+        log.debug("Bu tweet için like kaydediliyor: {} bu kullanıcı tarafından: {}", like.getTweet().getTweetId(), like.getUser().getUsername());
+        Optional<Like> existingLike = likeRepository.findByTweetTweetIdAndUserUserId(
+                like.getTweet().getTweetId(), 
+                like.getUser().getUserId()
+        );
+        
+        if (existingLike.isPresent()) {
+            log.debug("Zaten like var.");
+            return existingLike.get();
+        }
+        
         return likeRepository.save(like);
     }
 
     @Override
+    @Transactional
     public void unlikeTweet(Long tweetId, Long userId) {
+        log.debug("RBu tweet için like siliniyor: {} bu kullanıcı tarafından: {}", tweetId, userId);
         Optional<Like> like = likeRepository.findByTweetTweetIdAndUserUserId(tweetId, userId);
-        like.ifPresent(likeRepository::delete);
+        like.ifPresent(value -> {
+            log.debug("Like bulundu. Siliniyor.");
+            likeRepository.delete(value);
+        });
     }
 }

626f09f3c503d4a0d08a2ee9b7f8016a035eadc0
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-02-13 03:45:21 +0300

    change tweet service

--- src/main/java/com/example/twitter/service/TweetService.java
+++ src/main/java/com/example/twitter/service/TweetService.java
@@ -1,6 +1,7 @@
 package com.example.twitter.service;
 
 import com.example.twitter.entity.Tweet;
+import com.example.twitter.entity.User;
 
 import java.util.List;
 
@@ -8,6 +9,12 @@ public interface TweetService {
 
     Tweet createTweet(Tweet tweet);
 
+    List<Tweet> getAllTweets();
+
+    Tweet getTweetById(Long tweetId);
+
     List<Tweet> getTweetsByUserId(Long userId);
+
+    void deleteTweet(Long tweetId, User user);
 
 }
--- src/main/java/com/example/twitter/service/TweetServiceImpl.java
+++ src/main/java/com/example/twitter/service/TweetServiceImpl.java
@@ -1,16 +1,21 @@
 package com.example.twitter.service;
 
 import com.example.twitter.entity.Tweet;
+import com.example.twitter.entity.User;
 import com.example.twitter.repository.TweetRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
-public class TweetServiceImpl implements TweetService{
+public class TweetServiceImpl implements TweetService {
 
-    private TweetRepository tweetRepository;
+    private final TweetRepository tweetRepository;
 
     @Autowired
     public TweetServiceImpl(TweetRepository tweetRepository) {
@@ -18,12 +23,45 @@ public class TweetServiceImpl implements TweetService{
     }
 
     @Override
+    @Transactional
     public Tweet createTweet(Tweet tweet) {
+        log.debug("Tweet oluşturuluyor: {}", tweet);
         return tweetRepository.save(tweet);
     }
 
     @Override
+    @Transactional(readOnly = true)
+    public List<Tweet> getAllTweets() {
+        log.debug("Tüm tweetler çekiliyor");
+        return tweetRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Tweet getTweetById(Long tweetId) {
+        log.debug("Bu id ile tweet çekiliyor: {}", tweetId);
+        return tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new EntityNotFoundException("Bu id'de tweet bulunamadı: " + tweetId));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
     public List<Tweet> getTweetsByUserId(Long userId) {
+        log.debug("Bu id'li kullanıcı tweetleri çekiliyor: {}", userId);
         return tweetRepository.findByUserUserId(userId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteTweet(Long tweetId, User user) {
+        log.debug("Bu id'li tweet siliniyor: {} bu kullanıcı tarafından: {}", tweetId, user.getUsername());
+        Tweet tweet = getTweetById(tweetId);
+        
+        if (!tweet.getUser().getUserId().equals(user.getUserId()) && !user.getRole().name().equals("ROLE_ADMIN")) {
+            throw new IllegalStateException("Kullanıcının bu tweeti silmeye yetkisi yok");
+        }
+        
+        tweetRepository.delete(tweet);
+        log.debug("Tweet başarıyla silindi");
     }
 }
